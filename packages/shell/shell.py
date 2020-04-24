5e909f82b442731ad3590c536cd35de40820866c
Till Harbaum <till@harbaum.org>
2020-04-24 11:37:58 +0200

    Fix shell screen size

--- packages/shell/shell.py
+++ packages/shell/shell.py
@@ -4,25 +4,33 @@
 from TxtStyle import *
 import sys
 
+class ShellWidget(QLabel):
+    def __init__(self, parent = None):
+        QLabel.__init__(self, parent)
+        self.setStyleSheet("background-color: black");
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        self.setSizePolicy(sizePolicy)
+
+    def embed(self):
+        self.process = QProcess(self)
+        self.process.start(
+            'xterm',
+            ['-into', str(self.winId()),
+             "-geometry", "100x50" ]
+        )
+        
 class FtcGuiApplication(TouchApplication):
     def __init__(self, args):
         TouchApplication.__init__(self, args)
         # create the empty main window
         self.w = TxtWindow("Shell")
 
-        self.process  = QProcess(self.w)
-        self.terminal = QWidget(self.w)
-    
-        self.vbox = QVBoxLayout()
-        self.vbox.addWidget(self.terminal)
-        self.w.centralWidget.setLayout(self.vbox)
-
-        self.process.start(
-            'xterm',
-            ['-into', str(self.terminal.winId())]
-        )
-        
+        self.terminal = ShellWidget(self.w)
+        self.w.setCentralWidget(self.terminal)
         self.w.show()
+
+        self.terminal.embed()
+        
         self.exec_()
         
 if __name__ == "__main__":
